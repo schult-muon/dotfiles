5026e0e810ecf6287f86cd351c9f54a1b9a1d7d5
Steven Schulteis <schult@gmail.com>
2025-06-12 14:04:24 -0500

    Add documentation to init-dotfiles.sh

--- .local/bin/init-dotfiles.sh
+++ .local/bin/init-dotfiles.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+# https://www.atlassian.com/git/tutorials/dotfiles
 /usr/bin/git clone --bare git@github.com:schult/dotfiles.git "$HOME/.dotfiles"
 alias config='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 config config --local status.showUntrackedFiles no
+# https://morgan.cugerone.com/blog/workarounds-to-git-worktree-using-bare-repository-and-cannot-fetch-remote-branches/
 config config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
