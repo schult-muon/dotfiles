9a1b334b6947c6e39ea33faa7c3e10f8da0f8cfe
Steven Schulteis <schult@gmail.com>
2025-06-12 10:57:30 -0500

    Fix remote branches in config repo

--- .local/bin/init-dotfiles.sh
+++ .local/bin/init-dotfiles.sh
@@ -3,3 +3,4 @@
 /usr/bin/git clone --bare git@github.com:schult/dotfiles.git "$HOME/.dotfiles"
 alias config='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 config config --local status.showUntrackedFiles no
+config config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
